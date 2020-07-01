17557b7cab7d2f4b39a058d21407705cea2d86c0
Justin Phillips <jphillips@lyft.com>
2020-07-01 12:10:52 -0700

    ssl email send, use EmailMessage object

--- app/services/email.py
+++ app/services/email.py
@@ -1,16 +1,17 @@
-from smtplib import SMTP
+from email.message import EmailMessage
+from smtplib import SMTP_SSL
 from typing import Optional
 
 from app.settings import GMAIL_PASSWORD
 from app.settings import GMAIL_USER
 
-DEFAULT_EMAIL_CLIENT: Optional[SMTP] = None
+DEFAULT_EMAIL_CLIENT: Optional[SMTP_SSL] = None
 
 
-def _default_email_client() -> SMTP:
+def _default_email_client() -> SMTP_SSL:
     global DEFAULT_EMAIL_CLIENT
     if not DEFAULT_EMAIL_CLIENT:
-        DEFAULT_EMAIL_CLIENT = SMTP("smtp.gmail.com", 587)
+        DEFAULT_EMAIL_CLIENT = SMTP_SSL("smtp.gmail.com", 465)
     return DEFAULT_EMAIL_CLIENT
 
 
@@ -18,29 +19,24 @@ class EmailService:
 
     FROM_ADDRESS = "orders@wicksonrestaurant.com"
 
-    def __init__(self, email_client: Optional[SMTP] = None) -> None:
+    def __init__(self, email_client: Optional[SMTP_SSL] = None) -> None:
         self.email_client = email_client or _default_email_client()
+        self.email_client.login(GMAIL_USER, GMAIL_PASSWORD)
         return
 
-    def _create_email_body(self, customer_email_address: str, order_id: str) -> str:
-        return """\
-        From: %s
-        To: %s
-        Subject: Order %s
+    def _create_email_body(
+        self, customer_email_address: str, order_id: str,
+    ) -> EmailMessage:
+        msg = EmailMessage()
+        msg.set_content("This is my message")
 
-        Order received.
-        """ % (
-            self.FROM_ADDRESS,
-            customer_email_address,
-            order_id,
-        )
+        msg["Subject"] = f"Order {order_id} Receipt"
+        msg["From"] = self.FROM_ADDRESS
+        msg["To"] = customer_email_address
+        return msg
 
     def send_email(self, customer_email_address: str, order_id: str) -> None:
-        self.email_client.ehlo()
-        self.email_client.login(GMAIL_USER, GMAIL_PASSWORD)
-        self.email_client.sendmail(
-            self.FROM_ADDRESS,
-            customer_email_address,
+        self.email_client.send_message(
             self._create_email_body(
                 customer_email_address=customer_email_address, order_id=order_id,
             ),
