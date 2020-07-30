9b2c441023a75bb7b2b725f1f5a7c0bfc94b7b0a
Justin Phillips <jphillips@lyft.com>
2020-07-30 14:34:06 -0700

    checkin

---
+++ js/wickson/modal-loader.js
@@ -0,0 +1,8 @@
+function loadMainPageModal() {
+    var node = document.querySelector('#covid-modal');
+    if (node != null && node !== undefined) {
+        node.style.display = "block";
+    }
+}
+
+loadMainPageModal();
