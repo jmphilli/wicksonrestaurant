6c7fb4fab71ce74c4218776cb0fcea2c5a0c9da2
Justin Phillips <jphillips@lyft.com>
2020-07-30 15:03:54 -0700

    more

--- js/wickson/modal-loader.js
+++ js/wickson/modal-loader.js
@@ -3,6 +3,25 @@ function loadMainPageModal() {
     if (node != null && node !== undefined) {
         node.style.display = "block";
     }
+    var span = document.getElementsByClassName("close")[0];
+    if (span != null && span !== undefined) {
+        span.onclick = function () {
+            escapeHandler()
+        };
+    }
 }
+
+function escapeHandler() {
+    var node = document.querySelector('#covid-modal');
+    if (node != null && node !== undefined) {
+        node.style.display = "none";
+    }
+}
+
+$(document).keyup(function(e) {
+    if(e.key === "Escape") {
+        escapeHandler(e);
+    }
+});
 
 loadMainPageModal();
