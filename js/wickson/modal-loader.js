4db94272c725740bb36a81b97013aa95601ceb23
Justin Phillips <jphillips@lyft.com>
2020-07-30 15:37:46 -0700

    wew

--- js/wickson/modal-loader.js
+++ js/wickson/modal-loader.js
@@ -2,17 +2,37 @@ function loadMainPageModal() {
     var node = document.querySelector('#covid-modal');
     if (node != null && node !== undefined) {
         node.style.display = "block";
+    } else {
+        return;
     }
-    var span = document.getElementsByClassName("close")[0];
+    var elementId = '#close-covid-modal';
+    var span = document.querySelector(elementId)[0];
     if (span != null && span !== undefined) {
         span.onclick = function () {
-            escapeHandler()
+            escapeHandler(elementId)
         };
     }
 }
 
-function escapeHandler() {
-    var node = document.querySelector('#covid-modal');
+function loadOrderModalHandler() {
+    var node = document.querySelector('#order-link');
+    if (node != null && node !== undefined) {
+        node.onclick = function () {
+            var orderModal = document.querySelector('#order-modal');
+            orderModal.style.display = "block";
+        };
+    }
+    var elementId = "#close-order-modal";
+    var span = document.querySelector(elementId)[0];
+    if (span != null && span !== undefined) {
+        span.onclick = function () {
+            escapeHandler(elementId)
+        };
+    }
+}
+
+function escapeHandler(elementId) {
+    var node = document.querySelector(elementId);
     if (node != null && node !== undefined) {
         node.style.display = "none";
     }
@@ -20,8 +40,10 @@ function escapeHandler() {
 
 $(document).keyup(function(e) {
     if(e.key === "Escape") {
-        escapeHandler(e);
+        escapeHandler('#close-covid-modal');
+        escapeHandler('#close-order-modal'); // lol oops gotta do both
     }
 });
 
 loadMainPageModal();
+loadOrderModalHandler();
